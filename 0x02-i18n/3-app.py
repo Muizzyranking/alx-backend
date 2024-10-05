1c10c52f00a966883de942165b7a1891903ccfe8
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2024-10-05 03:16:42 +0100

    Parameterized templates
    Signed-off-by: Muizzyranking <oyebowaleabdulmuiz@gmail.com>

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+"""
+This module contains a Flask application with Babel integration
+for handling translations and timezone settings.
+"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel, gettext
+
+app = Flask(__name__)
+
+
+class Config:
+    """
+    Config class used to configure available
+    languages, default locale, and timezone.
+    """
+    LANGUAGES = ["en", "fr"]
+
+    BABEL_DEFAULT_LOCALE = "en"
+
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """
+    Determine the best match for supported languages from the request.
+
+    Uses the request's 'Accept-Language' headers to select
+    the best match from the supported languages.
+
+    Returns:
+        The best matching language as a string.
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def index():
+    """
+    The index function serves the homepage of the application.
+    It renders an HTML template called '2-index.html'.
+
+    Returns:
+        The rendered HTML template as a string.
+    """
+    return render_template("3-index.html", title=gettext("home_title"),
+                           header=gettext("home_header"))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
